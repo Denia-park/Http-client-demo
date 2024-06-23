6ebeb506f23731603b8670391c9f5d86018dd56c
hgpark <phg9587@naver.com>
2024-06-23 11:58:52 +0900

    Spring Boot3.2에 새롭게 추가될 RestClient
    - https://mangkyu.tistory.com/303

--- src/main/java/com/sample/restclientdemo/RestClientDemoApplication.java
+++ src/main/java/com/sample/restclientdemo/RestClientDemoApplication.java
@@ -6,7 +6,10 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
 import org.springframework.web.service.annotation.GetExchange;
@@ -15,7 +18,7 @@ import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 @SpringBootApplication
 public class RestClientDemoApplication {
 	@Bean
-	ApplicationRunner init(ErApi api) {
+	ApplicationRunner init(ErApi erApiWebClient, ErApi erApiRestClient) {
 		return args -> {
 			// https://open.er-api.com/v6/latest
 			RestTemplate rt = new RestTemplate();
@@ -41,14 +44,40 @@ public class RestClientDemoApplication {
 			Map<String, Map<String, Double>> response4 = erApi2.getLatest();
 			System.out.println(response4.get("rates").get("KRW"));
 
-			//Bean으로 등록해서 사용하는 방법
-			Map<String, Map<String, Double>> response5 = api.getLatest();
+			//HttpInterfact(WebClient) Bean으로 등록해서 사용하는 방법
+			Map<String, Map<String, Double>> response5 = erApiWebClient.getLatest();
 			System.out.println(response5.get("rates").get("KRW"));
+
+			RestClient restClient = RestClient.create();
+			// final String body = restClient.get().uri("https://open.er-api.com/v6/latest").retrieve().body(String.class);
+			final RestClient.ResponseSpec retrieve = restClient.get().uri("https://open.er-api.com/v6/latest").retrieve();
+
+			// ResponseEntity로 받을 수도 있다.
+			final ResponseEntity<String> entity = retrieve.toEntity(String.class);
+			System.out.println(entity.getStatusCode());
+			System.out.println(entity.getHeaders());
+			System.out.println(entity.getBody());
+
+			// 혹은 바로 Body를 읽어서 사용할 수도 있다.
+			// Map<String, Map<String, Double>> response6 = retrieve.body(Map.class);
+			// System.out.println(response6.get("rates").get("KRW"));
+
+			// JSON 변환도 가능하다.
+			// Pet pet = restClient.get().uri("https://petclinic.example.com/pets/{id}", id).accept(APPLICATION_JSON).retrieve().body(Pet.class);
+
+			// POST 요청
+			// Pet pet = ...
+			// ResponseEntity response = restClient.post().uri("https://petclinic.example.com/pets/new").contentType(APPLICATION_JSON).body(pet).retrieve().toBodilessEntity();
+
+			// HttpInterface(RestClient) Bean 사용 (WebClient 대신에 RestClient를 사용)
+			Map<String, Map<String, Double>> response7 = erApiRestClient.getLatest();
+			System.out.println("HttpInterface(RestClient): " + response7.get("rates").get("KRW"));
+
 		};
 	}
 
 	@Bean
-	ErApi erApi() {
+	ErApi erApiWebClient() {
 		WebClient client = WebClient.create("https://open.er-api.com");
 
 		HttpServiceProxyFactory factory = HttpServiceProxyFactory.builder(WebClientAdapter.forClient(client)).build();
@@ -56,9 +85,26 @@ public class RestClientDemoApplication {
 		return factory.createClient(ErApi.class);
 	}
 
+	@Bean
+	ErApi erApiRestClient() {
+		RestClient client = RestClient.create("https://open.er-api.com");
+		// RestClient client = RestClient.create();
+
+		HttpServiceProxyFactory factory = HttpServiceProxyFactory.builder()
+			.exchangeAdapter(RestClientAdapter.create(client))
+			.build();
+
+		return factory.createClient(ErApi.class);
+	}
+
 	interface ErApi{
+		//Client에서 BASE URL을 지정한 경우 interface에서는 뒤에 붙을 상세 주소만 지정해주면 됨
 		@GetExchange("/v6/latest")
 		Map getLatest();
+
+		//Client에서 BASE URL을 지정하지 않은 경우 interface에서 전체 URL을 지정하면 에러가 나지 않음
+		@GetExchange("https://open.er-api.com/v6/latest")
+		Map getLatestExceptNoUrlClient();
 	}
 
 	public static void main(String[] args) {
